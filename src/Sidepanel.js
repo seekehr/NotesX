96f0341f49c044ab1be71ec2beaf13ee57f2c982
Seeker <83727884+Sekkur@users.noreply.github.com>
2024-06-08 13:38:04 +0500

    ok

---
+++ src/Sidepanel.js
@@ -0,0 +1,13 @@
+function Sidepanel() {
+    private function closePanel() {
+
+    }
+
+    return (
+        <div className="sidepanel">
+            <a href="javascript:void(0)" className="closebtn" onClick={closePanel}>&times;</a>
+        </div>
+    );
+}
+
+export default Sidepanel;
