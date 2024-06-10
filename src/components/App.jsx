7977cd02bc1213a871f5db93450d32bdb345544c
seeker <83727884+seekehr@users.noreply.github.com>
2024-06-10 14:04:27 +0500

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react';
 import NoteTaker from './NoteTaker';
 
 function App() {
-  // TODO: save state for sidepanel toggled
-  const [isSPOpen, spToggle] = useState(false);
   return (
     <div className="App">
       <NoteTaker />
